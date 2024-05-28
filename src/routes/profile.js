2e74419086f6f8bf9e2787bda1955c2bb9832cbb
RafaFigueiroa <rafatinsfig@gmail.com>
2024-05-28 16:09:44 -0300

    CRUD de profile estruturado

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -4,30 +4,68 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/', (req, res) => {
-    return res.send(Object.values(req.context.models.personalInfos));
+    return res.send(Object.values(req.context.models.profiles));
 });
 
 router.post('/', (req, res) => {
     const id = uuid();
-    const personalInfo = {
+    const profile = {
         id,
         name: req.body.name,
         lastName: req.body.lastName,
         title: req.body.title,
-        about: req.body.about
+        about: req.body.about,
+        number: req.body.number,
+        location: req.body.location,
+        email: req.body.email,
+        github: req.body.github,
+        linkedin: req.body.linkedin
     };
 
-    req.context.models.personalInfos[id] = message;
+    req.context.models.profiles[id] = profile;
 
-    return res.send(personalInfo);
+    return res.send(profile);
 });
 
-router.put('/', (req, res) => {
-    res.send("atualizando as informações pessoais");
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    let profile = req.context.models.profiles[id];
+
+    if(!profile){
+        return res.send("Perfil não encontrado");
+    }
+
+    profile = {
+        id,
+        name: req.body.name,
+        lastName: req.body.lastName,
+        title: req.body.title,
+        about: req.body.about,
+        number: req.body.number,
+        location: req.body.location,
+        email: req.body.email,
+        github: req.body.github,
+        linkedin: req.body.linkedin
+    };
+
+    req.context.models.profiles[id] = profile;
+
+    return res.send(profile);
 });
 
-router.delete('/', (req, res) => {
-    res.send("deletando as informações pessoais");
+router.delete('/:id', (req, res) => {
+    const{
+        [req.params.id]: profile,
+        ...otherProfiles
+    } = req.context.models.profiles;
+
+    if(!profile){
+        return res.send("Perfil não encontrado");
+    }
+
+    req.context.models.profiles = otherProfiles;
+
+    return res.send(profile);
 });
 
 export default router;
